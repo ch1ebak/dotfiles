7a8048e10074384a6f3c6439cadd0d57a24a93b4
ch1ebak <92380391+ch1ebak@users.noreply.github.com>
2022-04-10 16:04:26 +0200

    Add files via upload

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,7 +7,8 @@ end
 export PATH="$HOME/.emacs.d/bin:$PATH"
 set -e fish_user_paths
 set -U fish_user_paths $HOME/.local/bin $HOME/Applications $fish_user_paths
-
+export PATH="$PATH:$HOME/.spicetify"
+fish_add_path /home/kd/.spicetify
 
 ### EXPORTS
 set -g fish_greeting
@@ -39,22 +40,11 @@ starship init fish | source
 
 ### ALIASES
 
-## Adblock
-alias adblock='doas /usr/local/bin/hblock'
-
-## Ani-cli
-alias anime='ani-cli -vq best'
-
 ## Bpytop
 alias bt='bpytop'
 
 ## Calendar
 alias cal='calcure'
-
-## Cds
-alias .1='cd ..'
-alias .2='cd ../..'
-alias .3='cd ../../..'
 
 ## Changing "ls" to "exa"
 alias ls='exa -al --color=always --group-directories-first' # my preferred listing
@@ -68,14 +58,10 @@ alias cp="cp -i"
 alias mv='mv -i'
 alias rm='rm -i'
 
-## Corona
-alias covidpl='corona poland'
-
 ## Emacs
-alias emacs='emacsclient -c -a 'emacs''
-alias em='emacsclient -nw'
 alias emacsd='/usr/bin/emacs --daemon &'
 alias doom='~/.emacs.d/bin/doom sync'
+alias doomup='~/.emacs.d/bin/doom upgrade'
 alias kemacs='killall emacs'
 
 ## grep
@@ -86,32 +72,11 @@ alias fgrep='fgrep --color=auto'
 ## grub
 alias grubreload='sudo grub-mkconfig -o /boot/grub/grub.cfg'
 
-## soccer-cli
-alias mcfc='doas soccer --team=MCFC --upcoming'
-
 ## Merge Xresources
 alias merge='xrdb -merge ~/.Xresources'
 
-## Mixer
-alias mixer='ncpamixer'
-
-## mirrors
-alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
-alias mirrord="sudo reflector --latest 50 --number 20 --sort delay --save /etc/pacman.d/mirrorlist"
-alias mirrors="sudo reflector --latest 50 --number 20 --sort score --save /etc/pacman.d/mirrorlist"
-alias mirrora="sudo reflector --latest 50 --number 20 --sort age --save /etc/pacman.d/mirrorlist"
-
 ## mkdir
 alias mkdir='mkdir -pv'
-
-## Monitors
-alias mboth='fish ~/.screenlayout/benq-dell.sh'
-
-## ncmpcpp
-alias music='ncmpcpp'
-
-## neovim
-alias vim='nvim'
 
 ## pacman and yay
 alias gp='sudo pacman -S'
@@ -119,43 +84,18 @@ alias rp='sudo pacman -R'
 alias gy='yay -S'
 alias ry='yay -Rns'
 alias pacsyu='sudo pacman -Syu'                 # update only standard pkgs
-alias yaysua='yay -Sua'              # update only AUR pkgs (yay)
 alias yaysyu='yay -Syu'              # update standard pkgs and AUR pkgs (yay)
-alias parsua='paru -Sua'             # update only AUR pkgs (paru)
-alias parsyu='paru -Syu'             # update standard pkgs and AUR pkgs (paru)
 alias unlock='sudo rm /var/lib/pacman/db.lck'    # remove pacman lock
 alias cleanup='sudo pacman -Rns (pacman -Qtdq)'  # remove orphaned packages
 alias packagelist='sudo pacman -Qe > packages.txt'
 
-## Ratesx
-alias ratesx='curl rate.sx'
-
 ## Root priviledges
 alias sudo='doas'
 
-## Todoist
-# alias tdl='todoist list'
-# alias tda='todoist add'
-# alias tdc='todoist close'
-# alias tdd='todoist delete'
-# alias tds='todoist sync'
-
 ## Terminal
 alias :q='exit'
-
-## youtube-dl
-# alias yta-aac="youtube-dl --extract-audio --audio-format aac "
-# alias yta-best="youtube-dl --extract-audio --audio-format best "
-# alias yta-flac="youtube-dl --extract-audio --audio-format flac "
-# alias yta-m4a="youtube-dl --extract-audio --audio-format m4a "
-# alias yta-mp3="youtube-dl --extract-audio --audio-format mp3 "
-# alias yta-opus="youtube-dl --extract-audio --audio-format opus "
-# alias yta-vorbis="youtube-dl --extract-audio --audio-format vorbis "
-# alias yta-wav="youtube-dl --extract-audio --audio-format wav "
-# alias ytv-best="youtube-dl -f bestvideo+bestaudio "
 
 ## Weather
 alias wpl='curl wttr.in/pleszew'
 alias wpo='curl wttr.in/poznan'
 alias wv='curl wttr.in/irvine'
-export PATH="$PATH:$HOME/.spicetify"
