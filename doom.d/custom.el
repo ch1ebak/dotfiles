704d08dc88584bd5ed8db90504a578923f32d145
ch1ebak <92380391+ch1ebak@users.noreply.github.com>
2022-05-09 22:59:57 +0200

    Add files via upload

---
+++ doom.d/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files '("~/Dokumenty/org/org-roam/20211206160944-org_agenda.org"))
+ '(safe-local-variable-values '((git-commit-major-mode . git-commit-elisp-text-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(font-lock-comment-face ((t (:slant italic))))
+ '(font-lock-keyword-face ((t (:slant italic)))))
