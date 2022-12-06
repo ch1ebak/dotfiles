26ff10ebe3cd7dee2444cb034f2b9c81287ad08f
ch1ebak <92380391+ch1ebak@users.noreply.github.com>
2022-12-06 18:02:23 +0100

    Add files via upload

---
+++ doom.d/packages.el
@@ -0,0 +1,64 @@
+;; A
+(package! all-the-icons)
+(package! all-the-icons-ivy-rich)
+(package! all-the-icons-completion)
+
+;; B
+(package! beacon)
+
+;; C
+(package! counsel-css)
+
+;; D
+(package! dash)
+(package! dashboard)
+(package! dimmer)
+(package! dired+)
+(package! dired-hacks)
+(package! dired-open)
+
+;; E
+(package! elfeed)
+(package! elfeed-org)
+(package! elfeed-goodies)
+(package! elpher)
+(package! emmet-mode)
+(package! todoist)
+(package! emacsql)
+(package! emacsql-sqlite)
+(package! evil-ledger)
+
+;; L
+(package! ledger-mode)
+
+;; M
+(package! mu4e-alert)
+
+;; N
+(package! nov)
+(package! npm-mode)
+
+;; O
+(package! org-auto-tangle)
+(package! org-mime)
+(package! org-notifications)
+(package! org-sticky-header)
+(package! org-super-agenda)
+(package! ox-pandoc)
+
+;; P
+(package! persp-mode)
+(package! pocket-reader)
+
+;; R
+(package! request)
+
+;; S
+(package! smtpmail)
+(package! spell-fu)
+
+;; V
+(package! visual-fill-column)
+
+;; W
+(package! web-mode)
