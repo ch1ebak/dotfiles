420724708d289dd5ac8c1b51d71b93142fd3051b
ch1ebak <92380391+ch1ebak@users.noreply.github.com>
2023-01-31 19:26:42 +0100

    Add files via upload

---
+++ doom.d/packages.el
@@ -0,0 +1,48 @@
+;; A
+(package! all-the-icons)
+(package! all-the-icons-ivy-rich)
+(package! all-the-icons-completion)
+
+;; B
+(package! beacon)
+
+;; C
+(package! counsel-css)
+
+;; D
+(package! dired+)
+(package! dired-hacks)
+(package! dired-open)
+
+;; E
+;; (package! emmet-mode)
+;; (package! emacsql)
+;; (package! emacsql-sqlite)
+(package! evil-ledger)
+
+;; L
+(package! ledger-mode)
+
+;; N
+;; (package! npm-mode)
+
+;; O
+(package! org-auto-tangle)
+(package! org-mime)
+(package! org-sticky-header)
+(package! org-super-agenda)
+(package! ox-pandoc)
+(package! ox-epub)
+
+;; P
+(package! pocket-reader)
+
+;; R
+(package! request)
+
+;; S
+(package! smtpmail)
+(package! spell-fu)
+
+;; V
+(package! visual-fill-column)
