cc383f6194ad20c7811abdf0a84ae11cc72310c3
ch1ebak <92380391+ch1ebak@users.noreply.github.com>
2024-10-21 14:48:39 +0200

    Delete config directory

--- config/qtile/autostart.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-xrandr --output HDMI-0 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --rate 144 --output DP-0 --mode 1920x1080 --pos 0x0 --rotate normal --rate 165
-/usr/bin/emacs --daemon &
-nm-applet &
-picom -b
-dunst &
-brightnessctl set 60%
-shuf -e -n1 $HOME/Obrazy/tapety/sg/* | xargs feh --bg-fill
--- config/qtile/wallpaper.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-shuf -e -n1 $HOME/Obrazy/tapety/sg/* | xargs feh --bg-fill
