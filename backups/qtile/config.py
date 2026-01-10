3ef397b74d9af02299a91f4387ddfd5c0e0261cf
ch1ebak <nfwmb@proton.me>
2026-01-10 15:55:15 +0100

    fixes two

---
+++ backups/qtile/config.py
@@ -0,0 +1,184 @@
+# ============================================================
+# ▗▄▄▄▖▗▄▄▄▖▗▄▄▄▖▗▖   ▗▄▄▄▖     ▗▄▄▖ ▗▄▖ ▗▖  ▗▖▗▄▄▄▖▗▄▄▄▖ ▗▄▄▖
+# ▐▌ ▐▌  █    █  ▐▌   ▐▌       ▐▌   ▐▌ ▐▌▐▛▚▖▐▌▐▌     █  ▐▌
+# ▐▌ ▐▌  █    █  ▐▌   ▐▛▀▀▘    ▐▌   ▐▌ ▐▌▐▌ ▝▜▌▐▛▀▀▘  █  ▐▌▝▜▌
+# ▐▙▄▟▙▖ █  ▗▄█▄▖▐▙▄▄▖▐▙▄▄▖    ▝▚▄▄▖▝▚▄▞▘▐▌  ▐▌▐▌   ▗▄█▄▖▝▚▄▞▘
+#
+# github.com/ch1ebak/dotfiles
+# ============================================================
+
+
+# IMPORTS
+import os
+import subprocess
+import re
+import socket
+from libqtile import extension, hook, layout, qtile
+from libqtile.config import Click, Group, Key, Match
+from libqtile.lazy import lazy
+from typing import List
+
+## Color scheme
+from themes.tokyonight import colors, screens, widget_defaults
+
+# SETTINGS
+## General
+auto_fullscreen = True
+auto_minimize = True
+bring_front_click = False
+cursor_warp = False
+dgroups_key_binder = None
+dgroups_app_rules = []
+focus_on_window_activation = "smart"
+follow_mouse_focus = False
+reconfigure_screens = True
+wmname = "LG3D"
+
+### Floating
+floats_kept_above = True,
+floating_layout = layout.Floating(
+    border_width = 2,
+    border_focus = colors[5],
+    border_normal = colors[2],
+    float_rules=[
+        *layout.Floating.default_float_rules,
+        Match(wm_class="confirmreset"),
+        Match(wm_class="makebranch"),
+        Match(wm_class="maketag"),
+        Match(wm_class="ssh-askpass"),
+        Match(title="branchdialog"),
+        Match(wm_class="pinentry-gtk"),
+        Match(wm_class="file-roller")
+    ]
+)
+
+## Autostart
+@hook.subscribe.startup_once
+def start_once():
+    qtile.cmd_spawn("xrandr --output HDMI-0 --primary --mode 1920x1080 --pos 1920x0 --rate 144 --output DP-0 --mode 1920x1080 --pos 0x0 --rate 165")
+    qtile.cmd_spawn("nm-applet &")
+    qtile.cmd_spawn("picom -b")
+    qtile.cmd_spawn("dunst &")
+    qtile.cmd_spawn("brightnessctl set 50%")
+    qtile.cmd_spawn("easyeffects --gapplication-service &")
+    qtile.cmd_spawn("/usr/bin/emacs --daemon &")
+    for p in processes:
+        subprocess.Popen(p)
+
+## Groups
+groups = [
+    Group("1", label="󰭹", layout='max', matches=[Match(wm_class=["ferdium"])]),
+    Group("2", label="󰈹", layout='max'),
+    Group("3", label="󰈚", layout='monadtall', matches=[Match(wm_class=["emacs"])]),
+    Group("4", label="󰅴", layout='monadtall', matches=[Match(wm_class=["ghostty"])]),
+    Group("5", label="󰝰", layout='monadtall', matches=[Match(wm_class=["Thunar"])]),
+    Group("6", label="󰕊", layout='monadtall', matches=[Match(wm_class=["calibre", "qbittorrent", "nwg-look", "NexusMods.App", "eartag"])]),
+    Group("7", label="󰓓", layout='max', matches=[Match(wm_class=["Steam", "steam", "lutris", "heroic"])]),
+    Group("8", label="󰓇", layout='max', matches=[Match(wm_class=["easyeffects"])]),
+    Group("9", label="󰕧", layout='max', matches=[Match(wm_class=["mpv"])])
+]
+
+## Layouts
+layout_theme = {
+    "border_width": 2,
+    "margin": 8,
+    "border_focus": colors[5],
+    "border_normal": colors[2]
+}
+
+layouts = [
+    layout.Max(**layout_theme),
+    layout.MonadTall(**layout_theme),
+    layout.MonadThreeCol(**layout_theme),
+    layout.RatioTile(**layout_theme)
+]
+
+## Scripts
+@lazy.function
+def maximize_by_switching_layout(qtile):
+    current_layout_name = qtile.current_group.layout.name
+    if current_layout_name == 'monadtall':
+        qtile.current_group.layout = 'max'
+    elif current_layout_name == 'max':
+        qtile.current_group.layout = 'monadtall'
+
+
+# KEYS
+## General
+mod = "mod4"
+prompt = "{0}@{1}: ".format(os.environ["USER"], socket.gethostname())
+
+## Keybindings
+keys = [
+     # Apps
+    Key([mod], "Return", lazy.spawn("ghostty")),
+    Key([mod], "w", lazy.spawn("zen-browser")),
+    Key([mod, "shift"], "w", lazy.spawn("zen-browser --private-window")),
+    Key([mod], "d", lazy.spawn("emacsclient -c -a 'emacs'")),
+    Key([mod], "a", lazy.spawn("emacsclient -c -a 'emacs' --eval '(dired nil)'")),
+    Key([mod, "shift"], "a", lazy.spawn("thunar")),
+
+    # Menu
+    Key([mod], "s", lazy.spawn("rofi -m 1 -show drun")),
+    Key([mod], "u", lazy.spawn("/home/karna/Projekty/scripts/rofi-utilities")),
+    Key([mod], "p", lazy.spawn("slock")),
+    Key([mod, "shift"], "p", lazy.spawn("rofi -m 1 -show power-menu -modi power-menu:~/.config/rofi/modules/rofi-power-menu")),
+
+    # App control
+    Key([mod], "q", lazy.window.kill()),
+    Key([mod, "shift"], "q", lazy.spawn("xkill")),
+    Key([mod, "control"], "r", lazy.restart()),
+    Key([mod, "control"], "q", lazy.shutdown()),
+
+    # Switch between screens (2 monitors)
+    Key([mod], "x", lazy.to_screen(0)),
+    Key([mod], "z", lazy.to_screen(1)),
+
+    # Scroll through groups
+    Key([mod], 'j', lazy.screen.prev_group(skip_managed=True, skip_empty=True)),
+    Key([mod], 'k', lazy.screen.next_group(skip_managed=True, skip_empty=True)),
+
+    # Move focus
+    Key([mod], "h", lazy.layout.down()),
+    Key([mod], "l", lazy.layout.next()),
+
+    # Move windows
+    Key([mod, "shift"], "h", lazy.layout.shuffle_left()),
+    Key([mod, "shift"], "l", lazy.layout.shuffle_right()),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down()),
+    Key([mod, "shift"], "k", lazy.layout.shuffle_up()),
+
+    # Resize windows
+    Key([mod], "n", lazy.layout.maximize()),
+    Key([mod, "shift"], "n", lazy.layout.normalize()),
+    Key([mod, "control"], "h", lazy.layout.grow_left()),
+    Key([mod, "control"], "l", lazy.layout.grow_right()),
+    Key([mod, "control"], "j", lazy.layout.shrink()),
+    Key([mod, "control"], "k", lazy.layout.grow()),
+
+    # Layouts
+    Key(["shift", "control"], "Return", lazy.layout.toggle_split()),
+    Key([mod, "shift"], "Tab", lazy.layout.flip()),
+    Key([mod], "Tab", lazy.next_layout()),
+    Key([mod], "Space", maximize_by_switching_layout()),
+    Key([mod, "shift"], "Space", lazy.window.toggle_fullscreen())
+]
+
+## Move windows between groups
+for i in groups:
+    keys.extend(
+        [
+            Key(
+                [mod],
+                i.name,
+                lazy.group[i.name].toscreen(),
+                desc="Switch to group {}".format(i.name),
+            ),
+            Key(
+                [mod, "shift"],
+                i.name,
+                lazy.window.togroup(i.name, switch_group=True),
+                desc="Switch to & move focused window to group {}".format(i.name),
+            ),
+        ]
+    )
